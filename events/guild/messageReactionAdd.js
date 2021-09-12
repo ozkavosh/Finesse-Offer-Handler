cfca46846a4d29d8ea18670d6579696e7d49ca29
ozkavosh <64430715+ozkavosh@users.noreply.github.com>
2021-09-12 00:40:01 -0300

    prueba rol aceptado

--- events/guild/messageReactionAdd.js
+++ events/guild/messageReactionAdd.js
@@ -11,8 +11,21 @@ module.exports = (Discord, client, MessageReaction, User) => {
 
         const partes = MessageReaction.message.content.trim().split(/ +/g);
 
+        if(partes[8]){
+            switch(partes[9]){
+                case 'dps': partes[9] = `<:dps:884119963571458068> DPS`;
+                        break;
+                    case 'heal': partes[9] = `<:heal:884119949776420895> HEAL`;
+                        break;
+                    case 'tank': partes[9] = `<:tank:884119949512175647> TANK`;
+                        break;
+                    case 'chest1': partes[9] = `<:chest1:884474242605908090> FUNNEL`;
+                        break;
+            }
+        }
+
         client.users.fetch(partes[6]).then(usuario => {
-            usuario.send('Fuiste aceptado para el carry!').catch(console.error);
+            usuario.send(`Fuiste aceptado para el carry ! Tu rol es: ${partes[9]}`}).catch(console.error);
         });
 
         MessageReaction.message.edit(`Aceptaste a ${partes[5]} para el carry`).catch(console.error);

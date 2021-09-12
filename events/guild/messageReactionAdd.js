a848ed1fffcd3d3d93398923ed4285077276e273
Augusto Silva <64430715+ozkavosh@users.noreply.github.com>
2021-09-12 04:38:27 +0000

    rolaceptado final

--- events/guild/messageReactionAdd.js
+++ events/guild/messageReactionAdd.js
@@ -13,19 +13,19 @@ module.exports = (Discord, client, MessageReaction, User) => {
 
         if(partes[8]){
             switch(partes[9]){
-                case 'dps': partes[9] = `<:dps:884119963571458068>`;  partes[10] = 'DPS';
+                case 'dps': partes[8] = `<:dps:884119963571458068>`;  partes[9] = 'DPS';
                         break;
-                    case 'heal': partes[9] = `<:heal:884119949776420895>HEAL`; partes[10] = 'HEAL';
+                    case 'heal': partes[8] = `<:heal:884119949776420895>HEAL`; partes[9] = 'HEAL';
                         break;
-                    case 'tank': partes[9] = `<:tank:884119949512175647>TANK`; partes[10] = 'TANK';
+                    case 'tank': partes[8] = `<:tank:884119949512175647>TANK`; partes[9] = 'TANK';
                         break;
-                    case 'chest1': partes[9] = `<:chest1:884474242605908090>FUNNEL`; partes[10] = 'FUNNEL';
+                    case 'chest1': partes[8] = `<:chest1:884474242605908090>FUNNEL`; partes[9] = 'FUNNEL';
                         break;
             }
         }
 
         client.users.fetch(partes[6]).then(usuario => {
-            usuario.send(`Fuiste aceptado para el carry ! Tu rol es: ${partes[9]}`).catch(console.error);
+            usuario.send(`Fuiste aceptado para el carry ! Tu rol es: ${partes[8]} ${partes[9]}`).catch(console.error);
         });
 
         MessageReaction.message.edit(`Aceptaste a ${partes[5]} para el carry`).catch(console.error);
